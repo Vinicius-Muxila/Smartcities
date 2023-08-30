82094818473929241534d4f974129776dc9297e8
Vinícius Henrique <120579281+Vinicius-Muxila@users.noreply.github.com>
2023-08-30 15:05:22 -0300

    Update Professores.java

--- smartcities/src/main/java/br/com/fiap/smartcities/Professores.java
+++ smartcities/src/main/java/br/com/fiap/smartcities/Professores.java
@@ -9,7 +9,7 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "professores")
+@Table(name = "tb_professores")
 public class Professores {
 	
 	@Id
